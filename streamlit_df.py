2e85ce92063e0e683c0891a5477a31e08bd169ba
S Nanda Kiran <129741286+SharabNandakiran@users.noreply.github.com>
2023-04-07 11:45:00 +0530

    Update streamlit_df.py

--- streamlit_df.py
+++ streamlit_df.py
@@ -18,8 +18,8 @@ st.write(df)
 st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
   
 df2 = pd.DataFrame(
-    np.randomrandn(200, 3),
+    np.random.randn(200, 3),
     columns = ['a','b','c'])
-c = alt.Chart(df2).mark_circle().encode(x='a', y='b', size='c', color='c',
-                                        tooltip=['a','b','c'])
+c = alt.Chart(df2).mark_circle().encode(
+    x='a', y='b', size='c', color='c',tooltip=['a','b','c'])
 st.write(c)
