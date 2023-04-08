c1104b289ceb2e4b594c483ccac84de6a19e0001
S Nanda Kiran <129741286+SharabNandakiran@users.noreply.github.com>
2023-04-08 13:29:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,8 @@
-#import snscrape.modules.twitter as sntwitter
 import streamlit as st
-import pandas as pd
-#import pymomgo
-#from pymongo import MongoClient
-from PIL import Image
-from datetime import date
-import json
 
+st.header("st.button")
 
-def main():
-    tweets = 0
-    st.title("Twitter Scraping")
-    menu = ["Home", "About", "Search", "Display", "Download"]
-    choice = st.sidebar.selectbox("Menu", menu)
+if st.button("say hello")
+    st.write("why are you say hello")
+else:
+    "Good bye"
