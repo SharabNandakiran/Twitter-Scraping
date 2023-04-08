23c3f92b979035ae3df8e6fb6ceb80e8168e6a41
S Nanda Kiran <129741286+SharabNandakiran@users.noreply.github.com>
2023-04-08 19:12:48 +0530

    Update streamlit_df.py

--- streamlit_df.py
+++ streamlit_df.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import altair as alt
-#import pandas as pd
+import pandas as pd
 #import pandas_profiling
 #from streamlit_pandas_profiling import st_profile_report
 
