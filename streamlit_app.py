b4b3900777e5666fa035364a8346eb4714625e50
S Nanda Kiran <129741286+SharabNandakiran@users.noreply.github.com>
2023-04-08 10:08:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,7 @@ if st.button('Say hello'):
      st.write('Why hello there')
 with st.form(key='Twitter_form'):
     search_term = st.text_input('User name')
+    search_term = st.text_input('Date')
+    limit = st.slider('How many tweets do you want to get?',0, 1000, step=20)
+    submit_button = st.form_submit_button(label='Search')
              
