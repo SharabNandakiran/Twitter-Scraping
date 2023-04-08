636b89e790d0583e42c0cbbaa92c678eeaf8e61b
S Nanda Kiran <129741286+SharabNandakiran@users.noreply.github.com>
2023-04-08 19:09:25 +0530

    Update streamlit_df.py

--- streamlit_df.py
+++ streamlit_df.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import numpy as np
 import altair as alt
-import pandas as pd
-import pandas_profiling
-from streamlit_pandas_profiling import st_profile_report
+#import pandas as pd
+#import pandas_profiling
+#from streamlit_pandas_profiling import st_profile_report
 
 
 st.header('st.write')
